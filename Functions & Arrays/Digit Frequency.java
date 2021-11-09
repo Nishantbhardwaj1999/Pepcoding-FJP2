536c347fda46983fa2ebde8ff02d190ae8aec5c4
Archit Aggarwal <archit.aggarwal023@gmail.com>
2021-11-09 15:59:21 +0530

    Update Digit Frequency.java

--- Functions & Arrays/Digit Frequency.java
+++ Functions & Arrays/Digit Frequency.java
@@ -1,1 +1,31 @@
-safsa
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int d = scn.nextInt();
+        int f = getDigitFrequency(n, d);
+        System.out.println(f);
+    }
+    
+    
+    public static int getDigitFrequency(int n, int d){
+        
+        int freq = 0;
+        
+        // For Extracting Digits
+        while(n > 0){
+            int digit = n % 10;
+            
+            if(digit == d){
+                freq++;
+            } 
+            
+            n = n / 10;
+        }
+        
+        return freq;
+    }
+}
